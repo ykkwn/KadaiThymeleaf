3c1689f82ed851a72a4cb3eb8912bd4551b044d9
ykkwn <monmon_ranma@yahoo.co.jp>
2023-02-28 18:33:49 +0900

    Lesson15の課題提出のためのクラス・ファイル一覧

---
+++ src/main/java/com/techacademy/InputController.java
@@ -0,0 +1,13 @@
+package com.techacademy;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class InputController {
+
+    @GetMapping("/input")
+    public String getInput() {
+        return "input";
+    }
+}
